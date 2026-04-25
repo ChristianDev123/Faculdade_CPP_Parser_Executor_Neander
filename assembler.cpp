6a11d1fa485f21fc15739d234037975064b464a0
ChristianDev123 <christian.lima999@gmail.com>
2026-04-25 20:38:25 -0300

    assembler primeira passagem

--- assembler.cpp
+++ assembler.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <string>
 #include <vector>
+#include <regex>
 
 using namespace std;
 
@@ -11,6 +12,43 @@ void read_file(std::ifstream &file, std::vector<std::string> &buffer){
         buffer.push_back(line);
 }
 
+class Symbol {
+    public:
+        std::string name;
+        int mem_add;
+        int size = 1;
+        int value = 0;
+};
+
+std::vector<Symbol> symbol_table(std::vector<std::string> code){
+    std::vector<Symbol> response;
+    int 
+        data_index = 0,
+        mem_add = 255;
+    /* Encontrando Diretiva de Dados*/
+    for(auto&line : code){
+        if(line.find(".DATA") || line.find(".data")) break;
+        data_index++;
+    }
+    
+    std::regex pattern(R"((\s*)(\w+)(\s+)(\w+)(\s+)(\d+)(\s*))");
+    std::smatch match;
+
+    while(!std::regex_search(code[++data_index], std::regex(R"(\.([a-z,A-Z]*))")) && data_index < code.size()){
+        /* MATCH serve como um buffer de cada encontro das diretivas do regex, match[0] string com ocorrencia do regex */
+        if(!std::regex_search(code[data_index], match, pattern))
+            std::cerr << "[ERRO] Falha encontrada ao verificar a estrutura de diretiva!";
+
+        Symbol s;
+        s.name = match[2];
+        s.mem_add = mem_add;
+        s.size = match[4].str() == "SPACE" || match[4].str() == "space" ? std::stoi(match[6].str()) : 1;
+        s.value = match[4].str() == "DATA" || match[4].str() == "data" ? std::stoi(match[6].str()) : 0; 
+        mem_add -= s.size;
+        response.push_back(s);
+    }
+    return response;
+}
 
 int main(){
     std::vector<std::string> buffer_code;
@@ -18,9 +56,16 @@ int main(){
     if(!source_code) return EXIT_FAILURE;
     read_file(source_code, buffer_code);
     source_code.close();
+    symbol_table(buffer_code);
+    for(auto symbol : symbol_table(buffer_code)){
+        std::cout << "=========================================" << std::endl;
+        std::cout << symbol.name << std::endl;
+        std::cout << symbol.mem_add << std::endl;
+        std::cout << symbol.size << std::endl;
+        std::cout << symbol.value << std::endl;
+        std::cout << "=========================================" << std::endl;
+    }
 
-    for(auto& line : buffer_code)
-        std::cout << line << std::endl;
     
     return EXIT_SUCCESS;
 }
