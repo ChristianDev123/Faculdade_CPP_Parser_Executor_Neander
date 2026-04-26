3180c01289dc81dd163541cf74bbddb110a2bf64
ChristianDev123 <christian.lima999@gmail.com>
2026-04-26 17:55:25 -0300

    comandos traduzidos

--- assembler.cpp
+++ assembler.cpp
@@ -4,7 +4,8 @@
 #include <vector>
 #include <regex>
 #include <unordered_map>
-
+#include <algorithm>
+#include <cctype>
 
 using namespace std;
 
@@ -22,6 +23,18 @@ class Symbol {
         int value = 0;
 };
 
+class Command {
+    public:
+        int opcode;
+        int mem_add;
+        int arg = 0;
+};
+
+std::string string_to_upper(std::string s){
+    std::transform(s.begin(), s.end(), s.begin(), ::toupper);
+    return s;
+}
+
 std::vector<Symbol> symbol_table(std::vector<std::string> code){
     std::vector<Symbol> response;
     int 
@@ -29,7 +42,7 @@ std::vector<Symbol> symbol_table(std::vector<std::string> code){
         mem_add = 255;
     /* Encontrando Diretiva de Dados*/
     for(auto&line : code){
-        if(line.find(".DATA") || line.find(".data")) break;
+        if(string_to_upper(line).find(".DATA") != std::string::npos) break;
         data_index++;
     }
     
@@ -44,39 +57,139 @@ std::vector<Symbol> symbol_table(std::vector<std::string> code){
         Symbol s;
         s.name = match[2];
         s.mem_add = mem_add;
-        s.size = match[4].str() == "SPACE" || match[4].str() == "space" ? std::stoi(match[6].str()) : 1;
-        s.value = match[4].str() == "DATA" || match[4].str() == "data" ? std::stoi(match[6].str()) : 0; 
+        s.size = string_to_upper(match[4].str()) == "SPACE" ? std::stoi(match[6].str()) : 1;
+        s.value = string_to_upper(match[4].str()) == "DATA" ? std::stoi(match[6].str()) : 0; 
         mem_add -= s.size;
         response.push_back(s);
     }
     return response;
 }
 
-
 std::unordered_map<string, int> enum_operations {
-    {"NOP", 0},
-    {"STA", 10},
-    {"LDA", 20},
-    {"ADD", 30},
-    {"OR", 50},
-    {"AND", 60},
-    {"NOT", 70},
-    {"JMP", 80},
-    {"JN", 90},
+    {"NOP", 0x0},
+    {"STA", 0x10},
+    {"LDA", 0x20},
+    {"ADD", 0x30},
+    {"OR", 0x50},
+    {"AND", 0x60},
+    {"NOT", 0x70},
+    {"JMP", 0x80},
+    {"JN", 0x90},
     {"JZ", 0xA0},
     {"HLT", 0xF0}
 };
+
+int sum_addrs(std::string s){
+    int response;
+    char opr;
+    std::smatch match;
+    if(std::regex_search(s, match, std::regex(R"(((\d+)(\s*(\+|\-))?\s*)+)"))){
+        response = std::stoi(match[1].str());
+        for(int i = 2; i < match.size(); i++){
+            if(i%2 == 0){
+                opr = match[i].str()[0];
+                continue;
+            }
+            response = opr == '+' ? 
+                response+std::stoi(match[i].str()): 
+                response-std::stoi(match[i].str());
+        }
+    }
+    return response;
+}
+
+std::vector<Command> translate_text(std::vector<std::string> code, std::vector<Symbol> symbol_table){
+    std::vector<Command> response;
+    int 
+        data_index = 0,
+        mem_add = 0;
+    
+    for(auto& line : code){
+        if(string_to_upper(line).find(".TEXT") != std::string::npos) break;
+        data_index++;
+    }
+    
+    // ^\s*(\w+) -> pega o comando
+    // \s+ -> obriga espaço
+    // (\[(.*)\]|([\w\d]+) -> aceita como argumento uma palavra/número ou estrutura [abc...]
+    // /s*$ -> obriga espaço ou nada ao fim do comando
+    std::regex pattern0(R"(^\s*(\w+)(?:\s+(?:\[(.*)\]|([\w\d]+)))?\s*$)");
+    std::regex pattern1(R"((^\s*)(\w+:)(\s*$))");
+
+    std::smatch match, cmd_match;
+    std::vector<std::string> cmd_arg_implicit = {"NOT","NOP","HLT"};
+    
+    data_index++;
+    
+    for(int i = data_index; i < code.size(); i++){
+        if(std::regex_search(code[i], std::regex(R"(\.([a-z,A-Z]*))"))) /* valida seção de texto do código*/
+            break;
+        if(std::regex_replace(code[i], std::regex(R"(\s)"),"").empty()) /* valida linha vazia */
+            continue;
+        
+        Command c;
+        if(std::regex_search(code[i],pattern1)){// em caso de label aponta para o proximo endereço de memória
+            c.opcode = enum_operations["NOP"];
+            c.mem_add = mem_add + 1;
+            response.push_back(c);
+            continue;
+        }
+        
+        if(!std::regex_search(code[i], match, pattern0)){ // verifica se os commandos estão seguindo um padrão esperado
+            std::cerr << "[ERRO] Falha ao tentar ler o código em .text; código: " << code[i] << " linha:  " << i << std::endl;
+            continue;
+        }
+        
+        if(string_to_upper(match[1].str()).find("ORG") != std::string::npos){ // informado o início do algoritmo
+            mem_add = std::stoi(match[3].str());
+            continue;
+        }
+        
+        c.opcode = enum_operations[string_to_upper(match[1].str())];
+        c.mem_add = mem_add++;
+        if(std::find(cmd_arg_implicit.begin(), cmd_arg_implicit.end(), string_to_upper(match[1].str())) == cmd_arg_implicit.end()){ // caso de comandos com argumentos explicitos
+            if(match[3].matched){// argumento sem a estrutura [abc..]
+                for(auto& sym : symbol_table){
+                    if(sym.name == match[3].str()){
+                        c.arg = sym.mem_add;
+                        break;
+                    }
+                }
+            }else if(match[2].matched){// argumento com a estrutura [abc..]
+                std::string calc = match[3];
+                for(auto& sym : symbol_table){
+                    size_t pos = match[3].str().find(sym.name);
+                    if(pos != std::string::npos){
+                        calc.replace(pos,sym.name.size(),std::to_string(sym.mem_add));
+                        break;
+                    }
+                }                
+                c.arg = sum_addrs(calc);
+            }
+            response.push_back(c);
+        }
+    }
+    return response;
+}
 
 int main(){
     std::vector<std::string> buffer_code;
     std::vector<Symbol> sym_table;
     std::ifstream source_code("code.asm");
+    std::vector<Command> translated_cmd_list;
+    
     if(!source_code) return EXIT_FAILURE;
     read_file(source_code, buffer_code);
     source_code.close();
     
     sym_table = symbol_table(buffer_code);
-
-    
+    translated_cmd_list = translate_text(buffer_code, sym_table);
+    for(auto& cmd : translated_cmd_list){
+        std::cout << "==================" << std::endl;
+        std::cout << "Comando: " << cmd.opcode << std::endl;
+        std::cout << "arg: " << cmd.arg << std::endl;
+        std::cout << "mem_addrs: " << cmd.mem_add << std::endl;
+        std::cout << "==================" << std::endl;
+    }
     return EXIT_SUCCESS;
 }
--- code.asm
+++ code.asm
@@ -1,33 +1,48 @@
 .data
     n1 DATA 1
+    n1n DATA 0
     counter DATA 10
     menos1 DATA 1
-    tabuada SPACE 10
+    mais1 DATA 1
+    tabuada SPACE 3
 .text
     ORG 0
 
     LDA menos1
     NOT
-    ADD menos1
+    ADD mais1
     STA menos1
     NOT
 
     LDA n1
-    STA [tabuada+counter]
-    LDA counter
-    ADD menos1
-    STA counter
+    NOT
+    ADD mais1
+    STA n1n
+    NOT
 
     loop:
-        LDA [tabuada + counter + 1]
+        LDA n1
         ADD n1
-        STA [tabuada + counter]
+        STA n1
         
         LDA counter
         ADD menos1
         JZ floop
+        STA counter
         JMP loop
 
-        STA counter
     floop:
+
+        LDA n1
+        ADD n1n
+        STA [tabuada]
+
+        LDA [tabuada]
+        ADD n1n
+        STA [tabuada + 1]
+
+        LDA [tabuada + 1]
+        ADD n1n
+        STA [tabuada + 1 + 1]
+
         HLT
